20d3c07973a8d91b8dd4f8b1f1479db6c76000ab
riccjohn <riccjohn@gmail.com>
2019-10-11 13:06:30 -0500

    Fix test descriptions, add test for json resopnse

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
-  test 'should get home' do
+  test 'should get /dummy endpoint' do
     get '/dummy'
     assert_response :success
   end
 
-  test 'home should respond with 200' do
+  test '/dummy endpoint should respond with 200' do
     get '/dummy'
     assert_equal 200, response.code.to_i
+  end
+
+  test '/dummy endpoint should respond with json data' do
+    get '/dummy'
+    dummy_response = JSON.parse(@response.body)
+    assert_equal 'bar', dummy_response['foo']
   end
 end
