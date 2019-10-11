dde80bad93ff8fd3e85353b2552bba8148a98d70
riccjohn <riccjohn@gmail.com>
2019-10-11 12:07:56 -0500

    Move endpoing to /dummy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   def dummy_api
     render json: {
-      foo: "bar"
+      foo: 'bar'
     }.to_json
   end
 end
--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
   test 'should get home' do
-    get root_url
+    get '/dummy'
     assert_response :success
   end
 
   test 'home should respond with 200' do
-    get root_url
+    get '/dummy'
     assert_equal 200, response.code.to_i
   end
 end
