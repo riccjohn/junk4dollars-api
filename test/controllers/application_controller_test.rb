3fc72b4a4b6d53dac72bb91d0cd24cb13d16ffe1
riccjohn <riccjohn@gmail.com>
2019-10-14 10:47:21 -0500

    Remove redundant test

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -6,11 +6,6 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test '/dummy endpoint should respond with 200' do
-    get '/dummy'
-    assert_equal 200, @response.code.to_i
-  end
-
   test '/dummy endpoint should respond with json data' do
     get '/dummy'
     dummy_response = JSON.parse(@response.body)
