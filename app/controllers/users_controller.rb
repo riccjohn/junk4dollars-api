1361b84d4dad4bf9f42365ac86c2cc979ba180d4
riccjohn <riccjohn@gmail.com>
2019-10-15 16:46:46 -0500

    Add User model methods to get data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    render json: @users
+    users = User.all_user_public_data
+    render json: users
   end
 
   def show
-    @user = User.find(params['id'])
-    render json: @user
+    user = User.single_user_public_data(params[:id])
+    render json: user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  def self.all_user_public_data
+    all.select(:id, :name, :created_at, :updated_at)
+  end
+
+  def self.single_user_public_data(id)
+    select(:id, :name, :created_at, :updated_at).find(id)
+  end
 end
