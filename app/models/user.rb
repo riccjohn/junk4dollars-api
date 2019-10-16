4825edfb8505c321886dfa696319f3f713fea22e
riccjohn <riccjohn@gmail.com>
2019-10-16 09:34:11 -0500

    Add serializer to only send back specific data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   def self.all_user_public_data
-    all.select(:id, :name, :created_at, :updated_at)
+    all
   end
 
   def self.single_user_public_data(id)
-    select(:id, :name, :created_at, :updated_at).find(id)
+    find(id)
   end
 end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,3 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :name, :created_at, :updated_at
+end
