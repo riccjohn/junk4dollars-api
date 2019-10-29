9d67d6011138d0ba0ee42607bd283da6aa2375ba
riccjohn <riccjohn@gmail.com>
2019-10-29 16:03:49 -0500

    Generate a new model for auctions

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_170613) do
+ActiveRecord::Schema.define(version: 2019_10_29_210303) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "auctions", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.decimal "start_bid", precision: 10, scale: 2
+    t.decimal "current_bid", precision: 10, scale: 2
+    t.binary "image"
+    t.datetime "end_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
