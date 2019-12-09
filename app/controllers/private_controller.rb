dd3d9bee2372af6d74a66ceb56c21b1c1ced46f5
riccjohn <riccjohn@gmail.com>
2019-12-09 10:12:44 -0600

    Remove temp routes / controllers for /public /private

--- app/controllers/private_controller.rb
+++
@@ -1,7 +0,0 @@
-class PrivateController < ActionController::API
-  include Secured
-
-  def private
-    render json: 'Hello from a private endpoint! You need to be authenticated to see this.'
-  end
-end
