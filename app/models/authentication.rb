7304cbb5a42a0d16a52b6e6acd3d85067e3699a8
riccjohn <riccjohn@gmail.com>
2019-12-17 14:40:10 -0600

    Reformat header to match real headers
    Reformat header to match real headers

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -5,9 +5,8 @@ class Authentication
   end
 
   def current_user(request)
-    # authorization: 'bearer token'
-    if request.headers['bearer']
-      bearer = request.headers['bearer']
+    if request.headers['authorization']
+      bearer = get_bearer_token(request.headers)
       begin
         user_to_retrieve = @parser.auth0_id(bearer)
         User.find_by_auth0_id(user_to_retrieve)
@@ -16,5 +15,14 @@ class Authentication
         return nil
       end
     end
+  end
+
+  private
+
+  def get_bearer_token(header)
+    auth = header['authorization']
+    auth_array = auth.split(' ')
+    bearer_index = auth_array.index('bearer')
+    auth_array[bearer_index + 1]
   end
 end
