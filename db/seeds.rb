15e53884d4d1266944682e05cfe1b8d506a47403
riccjohn <riccjohn@gmail.com>
2019-12-30 13:43:33 -0600

    Add more auctions and proper users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 users = [
   {
-    name: 'User 1'
+    name: 'John Riccardi',
+    auth0_id: 'auth0|5ddc4e5801825f0f008cf4c5'
   },
   {
-    name: 'User 2'
+    name: 'Polly Jones',
+    auth0_id: 'auth0|000000000000000000000000'
   }
 ]
 
@@ -27,6 +21,36 @@ auctions = [
     description: 'Brand new. Single card',
     starting_price: 1000,
     ends_at: '2019-11-01 15:35:21 -0500'
+  },
+  {
+    title: 'Garruk, Cursed Huntsman',
+    description: 'Throne of Eldraine | Mythic Rare | Regular Card (non-foil)',
+    starting_price: 449,
+    ends_at: '2020-01-03 16:00:00 -0500'
+  },
+  {
+    title: 'MTG Throne of Eldraine Brawl Decks (4)',
+    description: 'Set of all 4 ToE Brawl Decks',
+    starting_price: 7000,
+    ends_at: '2020-01-05 16:00:00 -0500'
+  },
+  {
+    title: 'Theros Beyond Death Booster Box',
+    description: 'New: A brand-new, unused, unopened, undamaged item (including handmade items).',
+    starting_price: 9499,
+    ends_at: '2020-01-24 00:00:00 -0500'
+  },
+  {
+    title: 'Lenovo ThinkPad X220',
+    description: 'Lenovo ThinkPad X220 Laptop i5-2410M 2.30GHz 4GB 500GB Win10Pro wifi',
+    starting_price: 7500,
+    ends_at: '2020-01-05 12:30:30 -0500'
+  },
+  {
+    title: 'ThinkPad T480 | i7-8650U | 512 GB SSD | 16 GB DDR4 RAM',
+    description: 'Item is in used condition and shows some wear and tear on the chassis and LCD. Laptop only, no power adapter.',
+    starting_price: 20000,
+    ends_at: '2020-01-06 14:00:00 -0500'
   },
 ]
 
