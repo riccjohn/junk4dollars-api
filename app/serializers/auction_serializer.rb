f6bf222ed942402ae72eae2316b870107ce7ad56
riccjohn <riccjohn@gmail.com>
2020-01-07 16:35:31 -0600

    Remove redundant attribute

--- app/serializers/auction_serializer.rb
+++ app/serializers/auction_serializer.rb
@@ -1,4 +1,4 @@
 class AuctionSerializer < ActiveModel::Serializer
   has_one :current_bid, key: :bid
-  attributes :id, :title, :description, :created_at, :ends_at, :user_id, :starting_price, :current_bid
+  attributes :id, :title, :description, :created_at, :ends_at, :user_id, :starting_price
 end
