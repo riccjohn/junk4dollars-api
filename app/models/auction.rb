0eb0aa37a8e19cef45b1402a80c517b56352786c
riccjohn <riccjohn@gmail.com>
2020-01-07 16:34:12 -0600

    Change name of bid to current_bid on auction model

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -2,7 +2,7 @@ class Auction < ApplicationRecord
   belongs_to :user
   has_many :bids
 
-  def bid
+  def current_bid
     bids.order(:created_at).last
   end
 end
--- app/serializers/auction_serializer.rb
+++ app/serializers/auction_serializer.rb
@@ -1,4 +1,4 @@
 class AuctionSerializer < ActiveModel::Serializer
-  has_one :bid
-  attributes :id, :title, :description, :created_at, :ends_at, :user_id, :starting_price, :bid
+  has_one :current_bid, key: :bid
+  attributes :id, :title, :description, :created_at, :ends_at, :user_id, :starting_price, :current_bid
 end
