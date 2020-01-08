dcefb07dec303f4049a2b4bd156c563ac506bf59
riccjohn <riccjohn@gmail.com>
2020-01-08 10:45:30 -0600

    Update users in tests to have auth0_id

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test 'should get /users/1 endpoint' do
-    new_user = User.create(name: 'Test')
+    new_user = User.create(name: 'Test', auth0_id: 'foo')
     get "/users/#{new_user['id']}"
     assert_response :success
   end
 
   test '/users/1 should retrieve a single user' do
     user_name = 'Maggie'
-    new_user = User.create(name: user_name)
+    new_user = User.create(name: user_name, auth0_id: 'foo')
     get "/users/#{new_user.id}"
 
     user_response = JSON.parse(@response.body, symbolize_names: true)
@@ -22,15 +22,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test '/users should retrieve all users as json' do
-    users = ['John', 'Polly']
-    users.each { |user| User.create(name: user)}
+
+    users = [
+      {name: 'John', auth0_id: '001'},
+      {name: 'Polly', auth0_id: '002'}
+    ]
+
+    users.each { |user| User.create(user)}
 
     get '/users'
 
     users_response = JSON.parse(@response.body, symbolize_names: true)
     response_names = users_response.map {|user| user[:name]}
 
-    assert_equal response_names, users
+    assert_equal response_names, users.map {|user| user[:name]}
     assert_equal 2, users_response.count
   end
 end
