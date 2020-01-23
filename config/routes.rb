05cd3a9f07d2253ec6ef98de31ce7408f2e82361
riccjohn <riccjohn@gmail.com>
2020-01-23 15:51:35 -0600

    Add route and sql query to get top users by bid #

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'user/me' => 'secure_users#me'
   get 'users' => 'users#index'
   get 'users/:id' => 'users#show'
+  get 'users/top/:count' => 'users#top'
   get 'auctions' => 'auctions#index'
   get 'auctions/all/details' => 'auctions#show_detail'
   get 'auctions/:id' => 'auctions#show'
